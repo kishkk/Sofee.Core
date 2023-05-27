c538506e0c3faf7d713e1a5184cfa3ada1be04cc
Kishor Kumar Kesavan Kadavil <Kishor.Kadavil@gds.ey.com>
2023-05-27 23:23:56 +0530

    Initial setup completed.

---
+++ Sofee.Core.API/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace Sofee.Core.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : RESTFulController
+    {
+        [HttpGet]
+        public ActionResult<string> Get() =>
+            Ok("Hello World!");
+    }
+}
